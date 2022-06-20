4d03de30ca47d64114bdaae89208172c6b4e93f6
Galvao <Galvao@DESKTOP-N53M1NE>
2022-06-19 23:02:58 -0300

    Teste

---
+++ JavaGit2/src/main/java/com/mycompany/javagit2/newJavaFile.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.javagit2;
+
+public class newJavaFile {
+
+   
+    public static void main(String args[]) {
+         for (int cc=0; cc<=100; cc +=10){
+            System.out.println(cc);
+        }
+
+// TODO code application logic here
+    }
+}
