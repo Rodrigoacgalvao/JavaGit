accd24020e3d082040117d4a8f4dc1a3ea70a00c
Galvao <Galvao@DESKTOP-N53M1NE>
2022-06-20 21:16:21 -0300

    Teste

--- JavaGit2/src/main/java/com/mycompany/javagit2/JavaGit2.java
+++ JavaGit2/src/main/java/com/mycompany/javagit2/JavaGit2.java
@@ -4,6 +4,6 @@ package com.mycompany.javagit2;
 public class JavaGit2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Teste do Git");
     }
 }
